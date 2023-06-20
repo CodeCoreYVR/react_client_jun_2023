6ba575e6ab11334174a2ffe8fc1403636006bf00
Taif Al Musabe <>
2023-06-20 13:53:01 -0500

    react client add hard codded QuestionDetails component as JSX

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const QuestionDetails = () => {
+  return(
+    <div>
+        <h2>What is your is favorite colour?</h2>
+        <p>Green, blue, red, etc....</p>
+        <p>By John Doe</p>
+        <p>
+          <small>Seen 10 times</small>
+          <small>Last editted 2 hrs ago</small>
+        </p>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // StrictMode is a development only component. We might get rid of this later. But what it does 
   // is, it will search through the React components that you write and it will tell you if there are 
   // things you need to be fix. It will give you hints
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
   // App here is the root component. That is because of how React is structured. There is always
   // one main component that renders out all of the rest of the components
   // So App is the very first component that render out all of the rest of the components we will have, and it will have children and
   // the children will have childern, etc. There is a tree of components
+
+  // We are not using strict mode for now
+  // With JSX, we had to access the React element, but with JSX and babbel we can
+  // now, write the component as a html tag
+  <QuestionDetails/>
+  // Note, with React and JSX, all tags must be closed
+  // for example, an html element self closing tag like <hr> might still work for regular html
+  // but for react, it must be closed like <hr/> or <hr></hr>
 );
 
 // If you want to start measuring performance in your app, pass a function
