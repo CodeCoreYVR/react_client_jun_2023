b5f21c13664f23e25a7f8cef2ef8d75ab816e0e3
Taif Al Musabe <>
2023-06-20 13:35:27 -0500

    react client remove unnecessary files and added notes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // StrictMode is a development only component. We might get rid of this later. But what it does 
+  // is, it will search through the React components that you write and it will tell you if there are 
+  // things you need to be fix. It will give you hints
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  // App here is the root component. That is because of how React is structured. There is always
+  // one main component that renders out all of the rest of the components
+  // So App is the very first component that render out all of the rest of the components we will have, and it will have children and
+  // the children will have childern, etc. There is a tree of components
 );
 
 // If you want to start measuring performance in your app, pass a function
